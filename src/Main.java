8061334bb08157fcb5619235e6f317dd900463fa
Alex <alexeym75@gmail.com>
2022-09-23 19:28:28 +0700

    Added homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EmployeeBook book = new EmployeeBook();
+
+        book.addEmployee("Daniil","Sergeyevich","123456",2,60_000);
+        book.addEmployee("William", "Henry", "112233", 1, 10_000);
+        book.addEmployee("Melinda", "French", "654321", 1, 8_000);
+        book.addEmployee("Jeffrey", "Preston", "333444", 1, 12_000);
+        book.addEmployee("Rafael","Nadal", "555678", 2,50_000);
+        book.addEmployee("Novak","Djokovic","999932",2,45_000);
+
+        System.out.println(book.findEmployee("123456"));
+        book.deleteEmployee("123111");
+        book.printEmployees();
+        book.printEmployeesByDep(2);
+        book.changeSalary("333444", 999000);
+        System.out.println(book.getEmployeesCount());
     }
 }
